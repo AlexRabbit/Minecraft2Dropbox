50aa261a56b6923405bb8e07cdd15e6443ce3cb1
Alex Cornejo <AlexRabbit@users.noreply.github.com>
2020-05-08 11:18:57 -0500

    Generating World...
    Generating World...

---
+++ Minecraft2Dropbox/source/Minecraft2Dropbox.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJH2B50ExOhxVXzihNHmzFblOuaXW5uSCoV8cUvZyXoDX3rGcbccB61b3O58u2Ro=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBNQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJiZksaHkrSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsGAlTMbiXqZg==
+::ZQ05rAF9IAHYFVzEqQIdIRVRTxaDOn/oIqcT+Or094o=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmLMm65FLQa/Lrf/eWXoEIFNA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCmLMm65FLQa/Lrf/eWXoEIFNA==
+::cRolqwZ3JBvQF1fEqQIdKRBRDAXCPmq5DaAMqOf9r8eOrEgeRuM5bM/P1fSqMu8D6Er8NZ8jlnsanMQZDhddPhGnZww/5w4=
+::dhA7uBVwLU+EWHiI8VoCKRlWRRDi
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmM4FYgO0EbAxOVKyWzCKYI6e/p7ufJoUIQG+MzfZeVzbGMLa9z
+::Zh4grVQjdCyDJGyX8VAjFD1oaAGRN3+1Fokx4eb+7PiGpFlPcPAwaI3UwvqLOOVz
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+xcopy /I /E %AppData%\.minecraft %HomeDrive%%HomePath%\Dropbox\Minecraft
+cd %AppData%
